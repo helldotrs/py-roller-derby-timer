e5ad012d8d7c34ce7fba30211a95a323081a4799
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-24 19:46:22 +0200

    creating basic timer
    source https://www.codespeedy.com/how-to-create-a-stopwatch-in-python/

--- timer.py
+++ timer.py
@@ -1,1 +1,18 @@
 import time
+
+def time_convert(secs):
+    mins    =   secs // 60 # // for int value instead of float
+    secs    =   secs %  60 
+    hours   =   mins %  60
+    mins    =   mins %  60
+    
+    print("Time lapsed = {0}:{1}:{2}".format(int(hours),int(mins),secs)) 
+
+input("Press [enter] to start")
+start_time  = time.time()
+
+input("Press [enter] to stop")
+end_time    = time.time()
+
+time_lapsed = end_time - start_time
+time_convert(time_lapsed)
